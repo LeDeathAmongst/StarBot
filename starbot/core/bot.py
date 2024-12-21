35023951556b14705b6a8bcfd628f00a78047d4d
Star Val <skylar.rae.val@proton.me>
2024-12-21 06:55:31 +0000

    Lowered shard count Since this is for the customized bot, I lowered the shard count as it won't be in many servers.

--- starbot/core/bot.py
+++ starbot/core/bot.py
@@ -96,7 +96,7 @@ class DynamicShardedBot(commands.GroupMixin, RPCMixin, dpy_commands.AutoShardedB
 
     def __init__(self, *args, cli_flags=None, bot_dir: Path = Path.cwd(), **kwargs):
         # Calculate the shard count dynamically
-        shard_count = kwargs.pop("shard_count", 3)
+        shard_count = kwargs.pop("shard_count", 1)
         kwargs["shard_count"] = shard_count
 
         super().__init__(*args, **kwargs)
